3fca1a509985bdfb9828e497c8c1d0de61b7c174
ShaanAbhi <abhinandanshaan@gmail.com>
2022-03-04 02:13:24 +0530

    function done

---
+++ main.js
@@ -0,0 +1,9 @@
+const addition = require("./add")
+const subtraction = require("./sub")
+const multipliction = require("./multiply")
+const division = require("./division")
+
+addition(4,2)
+subtraction(4,2)
+multipliction(4,2)
+division(4,2)
